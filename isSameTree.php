c2f728b4d8262fad9507027b2c9cdc114837fbdc
Keijiro_Ida <idatt1122@gmail.com>
2023-12-28 07:05:02 +0900

    20231228

---
+++ isSameTree.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q) {
+
+        if($p->val !== $q->val ) return false;
+        if($p === null && $q === null) return true;
+
+        $left = $this->isSameTree($p->left, $q->left);
+        $right = $this->isSameTree($p->right, $q->right);
+
+        return $left && $right;
+    }
+}
