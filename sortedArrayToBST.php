38048b1da8d6233c133090a2fa044fa312adb664
Keijiro_Ida <idatt1122@gmail.com>
2024-01-01 08:15:49 +0900

    20240101

---
+++ sortedArrayToBST.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return TreeNode
+     */
+    function sortedArrayToBST($nums) {
+
+        if($nums == null) return null;
+        $size = count($nums);
+        $mid = (int)($size/2);
+
+        $treeNode = new TreeNode($nums[$mid]);
+        $treeNode->left = $this->sortedArrayToBST(array_slice($nums, 0, $mid));
+        $treeNode->right = $this->sortedArrayToBST(array_slice($nums, $mid+1, $size));
+
+        return $treeNode;
+    }
+}
