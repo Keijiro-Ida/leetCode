4cedcbd56ddf06967c1620955f70da60b4b32134
Keijiro_Ida <idatt1122@gmail.com>
2024-01-19 05:33:30 +0900

    20240119

---
+++ hammingWeight.php
@@ -0,0 +1,17 @@
+<?php
+
+class Solution {
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function hammingWeight($n) {
+        $count = 0;
+
+        for($i=0; $i < 32; $i++) {
+            if($n & 1 == 1) $count++;
+            $n = $n >> 1;
+        }
+        return $count;
+    }
+}
