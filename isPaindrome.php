3434a227d6b38cbf73bd7c71a9aab89d6e463463
Keijiro_Ida <idatt1122@gmail.com>
2024-02-02 05:59:25 +0900

    20240202

---
+++ isPaindrome.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return Boolean
+     */
+    function isPalindrome($head) {
+        $values = [];
+
+        while($head) {
+            $values[] = $head->val;
+            $head = $head->next;
+        }
+
+        return $values == array_reverse($values);
+    }
+}
