81752fd00410d8773a5429a1960f5efb0ac45959
Keijiro_Ida <idatt1122@gmail.com>
2024-02-08 06:07:20 +0900

    20240209

---
+++ firstBadVersion.php
@@ -0,0 +1,26 @@
+<?php
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      public function isBadVersion($version){} */
+
+      class Solution extends VersionControl {
+        /**
+         * @param Integer $n
+         * @return Integer
+         */
+        function firstBadVersion($n) {
+            $start = 1;
+            $end = $n;
+
+            while($start <= $end) {
+                $mid = floor(($start + $end) /2);
+                if($this->isBadVersion($mid)) {
+                    $bad = $mid;
+                    $end = $mid - 1;
+                } else {
+                    $start = $mid + 1;
+                }
+            }
+            return $bad;
+        }
+    }
