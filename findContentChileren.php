946c313e00dd04f3ef06a6a45be9e969a7c83bde
Keijiro_Ida <idatt1122@gmail.com>
2024-03-07 06:27:49 +0900

    20240307

---
+++ findContentChileren.php
@@ -0,0 +1,27 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $g
+     * @param Integer[] $s
+     * @return Integer
+     */
+    function findContentChildren($g, $s) {
+        sort($g);
+        sort($s);
+
+        $count = 0;
+        $i = 0;
+        $j = 0;
+
+        while($i < count($g) && $j < count($s)) {
+            if($g[$i] <= $s[$j]) {
+                $count++;
+                $i++;
+            }
+            $j++;
+        }
+        return $count;
+    }
+}
