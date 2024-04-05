0032dbfd7ddf152a04faf296e138ec024141a0f9
Keijiro_Ida <idatt1122@gmail.com>
2024-04-06 07:13:23 +0900

    20240406

---
+++ postorder.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Definition for a Node.
+ * class Node {
+ *     public $val = null;
+ *     public $children = null;
+ *     function __construct($val = 0) {
+ *         $this->val = $val;
+ *         $this->children = array();
+ *     }
+ * }
+ */
+
+ class Solution {
+    /**
+     * @param Node $root
+     * @return integer[]
+     */
+    function postorder($root) {
+        $result = [];
+        if($root == null) return $result;
+
+        $result = $this->reverseOrder($root, $result);
+        return $result;
+    }
+
+    function reverseOrder($node, &$result) {
+        if($node == null) return;
+
+        foreach($node->children as $child) {
+            $this->reverseOrder($child, $result);
+        }
+        $result[] = $node->val;
+
+        return $result;
+    }
+}
