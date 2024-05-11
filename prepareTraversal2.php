e1f98291f2c893cacc7cefe6bd7f9b83031133a8
Keijiro_Ida <idatt1122@gmail.com>
2024-05-12 06:51:14 +0900

    20240512

---
+++ prepareTraversal2.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function preorderTraversal($root) {
+
+        $result = [];
+        $this->helper($root, $result);
+        return $result;
+    }
+
+    function helper($node, &$result) {
+
+        if($node === null) return;
+        $result[] = $node->val;
+
+        $this->helper($node->left, $result);
+        $this->helper($node->right, $result);
+    }
+}
