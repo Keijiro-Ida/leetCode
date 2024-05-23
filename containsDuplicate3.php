c2b58c176db446aa1eacd30d4b813a4a45b1790d
Keijiro_Ida <idatt1122@gmail.com>
2024-05-24 05:46:46 +0900

    20240524

---
+++ containsDuplicate3.php
@@ -0,0 +1,17 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        sort($nums);
+
+        for($i = 0; $i < count($nums)-1; $i++) {
+            if($nums[$i] == $nums[$i+1]) return true;
+        }
+        return false;
+    }
+}
