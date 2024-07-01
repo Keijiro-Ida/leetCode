c0f18a56a199db1f8078af0a2f611261e73bec2a
Keijiro_Ida <idatt1122@gmail.com>
2024-07-02 06:03:22 +0900

    20240702

---
+++ hammingDistance.php
@@ -0,0 +1,23 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @param Integer $y
+     * @return Integer
+     */
+    function hammingDistance($x, $y) {
+
+        $xor = $x ^ $y;
+
+        $distance = 0;
+
+        while($xor != 0) {
+             $distance += $xor & 1;
+             $xor >>= 1;
+        }
+
+        return $distance;
+    }
+}
