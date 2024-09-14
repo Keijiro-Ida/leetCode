74027c5909bd090d49d8a75ba8ecb66382c71595
Keijiro_Ida <idatt1122@gmail.com>
2024-09-15 06:34:23 +0900

    20240915

---
+++ isRectangleOverlap.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $rec1
+     * @param Integer[] $rec2
+     * @return Boolean
+     */
+    function isRectangleOverlap($rec1, $rec2) {
+
+        if($rec1[0] >= $rec2[2] ||
+           $rec1[2] <= $rec2[0] ||
+           $rec1[1] >= $rec2[3] ||
+           $rec1[3] <= $rec2[1]) {
+                return false;
+        }
+
+        return true;
+    }
+}
