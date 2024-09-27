373a2677ed58f6aae475044caa176ddcc9be2cad
Keijiro_Ida <idatt1122@gmail.com>
2024-09-28 06:47:07 +0900

    20240928

---
+++ increasingBST.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function increasingBST($root) {
+
+        $tmp = new TreeNode(0);
+        $node = $tmp;
+        $this->inorder($root, $node);
+        return $tmp->right;
+    }
+
+    function inorder($root, &$node) {
+        if($root == null) return;
+
+        $this->inorder($root->left, $node);
+
+        $node->right = new TreeNode($root->val);
+        $node = $node->right;
+
+        $this->inorder($root->right, $node);
+
+    }
+}
