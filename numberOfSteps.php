c4257bdbc84f6b8cd0b812ccf7926b8e3e57f2b9
Keijiro_Ida <idatt1122@gmail.com>
2024-12-08 08:20:33 +0900

    20241208

---
+++ numberOfSteps.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return Integer
+     */
+    function numberOfSteps($num) {
+        $count=0;
+        while($num > 0) {
+            if($num % 2 == 0) {
+                $num = intdiv($num, 2);
+            } else {
+                $num -= 1;
+            }
+            $count++;
+        }
+        return $count;
+    }
+}
