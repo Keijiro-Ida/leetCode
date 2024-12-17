2114bdb3f326acabc953b9495585a6e6d11026e1
Keijiro_Ida <idatt1122@gmail.com>
2024-12-18 06:12:58 +0900

    20241218

---
+++ createTargetArray.php
@@ -0,0 +1,19 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer[] $index
+     * @return Integer[]
+     */
+    function createTargetArray($nums, $index) {
+        $target = [];
+
+        for($i = 0; $i < count($nums); $i++) {
+            array_splice($target, $index[$i], 0, $nums[$i]);
+        }
+
+        return $target;
+    }
+}
