93e062b2a7cd724d0999a9b502fd1e709203fb35
Keijiro_Ida <idatt1122@gmail.com>
2025-01-03 18:02:46 +0900

    20250103

---
+++ shuffle.php
@@ -0,0 +1,20 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $n
+     * @return Integer[]
+     */
+    function shuffle($nums, $n) {
+        $result = [];
+
+        for($i = 0; $i < $n; $i++) {
+            $result[] = $nums[$i];
+            $result[] = $nums[$i + $n];
+        }
+
+        return $result;
+    }
+}
